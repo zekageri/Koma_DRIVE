c8929ad24a5aba1f6c04a32bdd78be8434329c71
Dr.Random <drsedate@gmail.com>
2020-11-15 09:26:37 +0100

    backendlogin

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
 const express = require('express')
-const app = express()
-const port = 3000
+const app     = express()
+const port    = 3000
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/FrontEnd/Login/Login.html');
-})
+  app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/FrontEnd/Login/Login.html');
+  })
 
-app.get('/Login', (req, res) => {
-  res.sendFile(__dirname + '/FrontEnd/Drive/Drive.html');
-})
-
-app.post('/KomaLogin', (req, res) => {
-  if(req.body != 0){
+/** LOGIN  */
+  app.post('/KomaLogin', (req, res) => {
     console.log(req.body);
-  }
-})
+    res.sendFile(__dirname + '/FrontEnd/Drive/Drive.html');
+  })
+  app.get('/Login.css', (req, res) => {
+    res.sendFile(__dirname + './FrontEnd/Login/Login.css');
+  })
+/** LOGIN  */
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
