f5efd2a4b64195a0f47f275fa1156ed3d7eee582
Dr.Random <drsedate@gmail.com>
2020-11-15 10:09:06 +0100

    Full login and notify js support with started folder and file fetch

---
+++ FrontEnd/SharedScripts/OwnNotify.js
@@ -0,0 +1,37 @@
+var Notify = function(message,title,type,timer){
+    var enteranim,exitanim;
+    if(type == "warning" || type == "danger"){
+       enteranim = 'animated bounceIn';
+       exitanim = 'animated bounceOut';
+    }else{
+        enteranim = 'animated fadeInDown';
+        exitanim = 'animated fadeOutRight';
+    }
+    $.notify({
+        title: '<strong>'+title+'</strong><br>',
+        message: message,
+        icon: 'glyphicon glyphicon-ok',
+        target: '_blank'
+    },{
+        element: 'body',
+        type: type,
+        showProgressbar: false,
+        placement: {from: "top",align: "right"},
+        offset: 20,
+        spacing: 10,
+        z_index: 1031,
+        delay: 3300,
+        timer: timer,
+        url_target: '_blank',
+        mouse_over: null,
+        animate: {
+            enter: enteranim,
+            exit: exitanim
+        },
+        onShow: null,
+        onShown: null,
+        onClose: null,
+        onClosed: null,
+        icon_type: 'class',
+    });
+}
--- app.js
+++ app.js
@@ -1,6 +1,37 @@
-const express = require('express')
-const app     = express()
-const port    = 3000
+const express = require('express');
+var fs        = require('fs');
+const app     = express();
+const port    = 3000;
+const klawSync  =   require('klaw-sync')
+var mysql       =   require('mysql');
+const path      =   require('path');
+
+app.use(express.urlencoded());
+app.use(express.json());
+
+var Restricted_Komas = {
+  "Dr.Random" : "Mrrandom0829",
+};
+
+function Fetch_Dirs(){
+  var items = [];
+  var paths = klawSync('./shared/drive');
+  for(var i = 0; i < paths.length; i++){
+      items.push(paths[i].path);
+  }
+  console.log("Dir items: ", items);
+  return items;
+}
+
+function Search_Koma(user,pw){
+  var IsFound = false;
+  for (const [key, value] of Object.entries(Restricted_Komas)) {
+    if(key == user && value == pw){
+      IsFound = true;
+    }
+  }
+  return IsFound;
+}
 
   app.get('/', (req, res) => {
     res.sendFile(__dirname + '/FrontEnd/Login/Login.html');
@@ -8,13 +39,39 @@ const port    = 3000
 
 /** LOGIN  */
   app.post('/KomaLogin', (req, res) => {
-    console.log(req.body);
-    res.sendFile(__dirname + '/FrontEnd/Drive/Drive.html');
+    if( Search_Koma(req.body.username,req.body.pw) ){
+      res.send("Success");
+    }else{
+      res.send("Nope");
+    }
   })
   app.get('/Login.css', (req, res) => {
-    res.sendFile(__dirname + './FrontEnd/Login/Login.css');
+    res.sendFile(__dirname + '/FrontEnd/Login/Login.css');
   })
 /** LOGIN  */
+
+/** IMAGES  */
+app.get('/AppIcon.png', (req, res) => {
+  res.sendFile(__dirname + '/FrontEnd/images/AppIcon.png');
+})
+/** IMAGES  */
+
+/** DRIVE  */
+app.get('/MainPage', (req, res) => {
+  res.sendFile(__dirname + '/FrontEnd/MainPage/index.html');
+})
+
+app.get('/GetFolders', (req, res) => {
+  res.send(Fetch_Dirs());
+})
+
+/** DRIVE  */
+
+/** SHARED SCRIPTS  */
+app.get('/OwnNotify.js', (req, res) => {
+  res.sendFile(__dirname + '/FrontEnd/SharedScripts/OwnNotify.js');
+})
+/** SHARED SCRIPTS  */
 
 
 app.listen(port, () => {
